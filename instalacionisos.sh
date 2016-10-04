2e341eef833345f8f9a234c738c21ef21d05c370
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2016-10-04 20:15:12 +0200

    Add files via upload

---
+++ instalacionisos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+while true
+do
+echo "Bienvenido al instalador de sistemas operativos en pendrive"
+echo "Indica dónde está la iso a intalar"
+echo "Si quires salir pulsa la tecla p "
+read ruta
+	if [ $ruta = "p"  ]
+		then			exit
+	elif [ $ruta !="" ]
+		then
+			echo "Oiga, introduzca la ruta"
+	elif [[ ( -n $ruta ) &&  ( -f  $ruta ) ]]
+		#Aquí comprobamos que la variable ruta no esté vacía y sea la ruta exacta donde cargar la imagen del sistema operativo a instalar.
+		then
+			echo "Ahora introduce la ruta donde está el pendrive"
+				read rutapen
+					if [[ ( -n $rutapen ) && ( -d $rutapen )]]
+						the
+							`dd if=$ruta of=$rutapen`
+							#Aquí ejecutamos la instrucción que permite grabar las isos booteables
+					else 
+						echo "La ruta no es correcta"
+					fi
+	fi
+			
+done
