de50054da8a35a552efbb6d726219b25089cd9cf
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2016-10-04 22:25:27 +0200

    Errores sintácticos
    Sigue dando fallo en columan13 , a la espera de ")"

--- instalacionisos.sh
+++ instalacionisos.sh
@@ -5,23 +5,24 @@ echo "Bienvenido al instalador de sistemas operativos en pendrive"
 echo "Indica dónde está la iso a intalar"
 echo "Si quires salir pulsa la tecla p "
 read ruta
-	if [ $ruta = "p"  ]
-		then			exit
-	elif [ $ruta !="" ]
-		then
-			echo "Oiga, introduzca la ruta"
-	elif [[ ( -n $ruta ) &&  ( -f  $ruta ) ]]
-		#Aquí comprobamos que la variable ruta no esté vacía y sea la ruta exacta donde cargar la imagen del sistema operativo a instalar.
-		then
-			echo "Ahora introduce la ruta donde está el pendrive"
-				read rutapen
-					if [[ ( -n $rutapen ) && ( -d $rutapen )]]
-						the
-							`dd if=$ruta of=$rutapen`
-							#Aquí ejecutamos la instrucción que permite grabar las isos booteables
-					else 
-						echo "La ruta no es correcta"
-					fi
-	fi
-			
+        if [ $ruta == "p"  ]
+                then                    exit
+        elif [ $ruta !="" ]
+                then
+                        echo "Oiga, introduzca la ruta"
+        elif [[ ( -n "$ruta" ) &&  ( -f "$ruta" ) ]]
+                #Aquí comprobamos que la variable ruta no esté vacía y sea la ruta exacta donde cargar la imagen del sistema operativo a instalar.
+                then
+                        echo "Ahora introduce la ruta donde está el pendrive"
+                                read rutapen
+                                        if [[ ( -n "$rutapen" ) && ( -d "$rutapen" ) ]]
+                                                then
+                                                        `dd if=$ruta of=$rutapen`
+                                                        #Aquí ejecutamos la instrucción que permite grabar las isos booteables
+                                        else
+                                                echo "La ruta no es correcta"
+                                        fi
+        fi
+
 done
+~                                                          
