aaaabdf6eb9a5e1b5ae906524da43d339f0f0a9a
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2016-10-05 17:49:39 +0200

    Ahora sólo falla en el comando
    No soy capaz de aplicar bien el comando dd

--- instalacionisos.sh
+++ instalacionisos.sh
@@ -8,21 +8,21 @@ read ruta
         if [ $ruta = "p"  ]
                 then
                         exit
-        elif [[ -z "$ruta" ]
-                then 
-                         echo "Oiga, introduzca la ruta"
-                 elif [ ( -n "$ruta" ) &&  ( -f "$ruta" ) ]]
-                       #Aquí comprobamos que la variable ruta no esté vacía y sea la ruta exacta donde cargar la imagen del sistema operativo a instalar.
+        elif [ -z $ruta ]
                 then
-                        echo "Ahora introduce la ruta donde está el pendrive"
-                                read rutapen 
-                                         if [[ ( -n "$rutapen" ) && ( -d "$rutapen" ) ]]
-                                                then
-                                                        `dd if=$ruta of=$rutapen` 
-                                                        #Aquí ejecutamos la instrucción que permite grabar las isos booteables
-                                                else
-                                                echo "La ruta no es correcta"
-                                        fi      
+                         echo "Oiga, introduzca la ruta"
+                 elif [ -f $ruta ]
+                         #Aquí comprobamos que la variable ruta no esté vacía y sea la ruta exacta donde cargar la imagen del sistema operativo a instalar.
+                         then
+                                echo "Ahora introduce la ruta donde está el pendrive"
+                                        read rutapen
+                                                 if [[ ( -n "$rutapen" ) && ( -d "$rutapen" ) ]]
+                                                        then
+                                                                dd if=$rutapen of=$ruta bs=4094 conv=noerror
+                                                                #Aquí ejecutamos la instrucción que permite grabar las isos booteables
+                                                        else
+                                                                echo "La ruta no es correcta"
+                                                fi
         fi
 
 done
